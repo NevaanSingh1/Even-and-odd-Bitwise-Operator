8530373add06a3c198ddb95c146673f2c68d075a
NevaanSingh1 <NevaanSinghmails@gmail.com>
2026-01-20 20:28:55 +1100

    Add files via upload

---
+++ Bitwise operator.py
@@ -0,0 +1,8 @@
+num1 = 42
+num2 = 3
+print("num1&num2=",num1 & num2)  # AND operator
+print("\nnum1 num2=",num1 | num2)  # OR operator
+print("\n~num1=", ~num1)  # NOT operator
+print("\nnum1^num2=", num1 ^ num2)  # XOR operator
+print("\nnum1<<2=", num1 << 2)  # Left Shift operator
+print("\nnum2>>2=", num2 >> 2)  # Right Shift operator
---
+++ Even and Odd.py
@@ -0,0 +1,12 @@
+def isEvenOdd(n):
+    if(n^1==n+1):
+        return True
+    else:
+        return False
+    
+
+number = int(input("Enter your number:"))
+if(isEvenOdd(number)):
+    print("The number is Even")
+else:
+    print("The number is Odd")
